e06f5ec4c3fb4a32116db4d851172f29e4744970
PKwhiting <patten.whiting@gmail.com>
2023-02-09 13:19:51 -0700

    adding data validation and error handling

--- controllers/users.js
+++ controllers/users.js
@@ -4,82 +4,118 @@ const ObjectId = require('mongodb').ObjectId;
 
 
 const getAllUsers = async (req, res, next) => {
-  let db = await mongodb.getDb()
-  const result = db.db("CarRentalApp").collection('users').find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+  try {
+    let db = await mongodb.getDb()
+    const result = db.db("CarRentalApp").collection('users').find();
+    const lists = await result.toArray();
+    if (!lists) {
+      res.status(404).send('No users found');
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error fetching users" });
+  }
 };
-
 
 const getSingleUser = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
-  let db = await mongodb.getDb()
-  const result = db.db("CarRentalApp").collection('users').find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const userId = new ObjectId(req.params.id);
+    let db = await mongodb.getDb()
+    const result = db.db("CarRentalApp").collection('users').find({ _id: userId });
+    const lists = await result.toArray();
+    if (!lists || lists.length === 0) {
+      res.status(404).send('User not found');
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error fetching user" });
+  }
 };
-
 
 const createNewUser = async (req, res) => {
   try {
     const { firstName, lastName, username, password, hoursRemaining } = req.body;
+    if (!firstName || !lastName || !username || !password || !hoursRemaining) {
+      return res.status(400).send("Bad Request: missing required fields");
+    }
     const contact = { firstName, lastName, username, password, hoursRemaining };
     let db = await mongodb.getDb();
     const result = await db.db("CarRentalApp").collection('users').insertOne(contact);
     res.status(201).json({ _id: result.insertedId });
   } catch (error) {
-    res.status(500).json(error).send();
+    console.log(error);
+    res.status(500).json({ error: "Error creating user" });
   }
 };
 
+const updateCurrentUser = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    if (!ObjectId.isValid(userId)) {
+      res.status(400).send("Invalid user ID");
+      return;
+    }
 
-  const updateCurrentUser = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
     const { firstName, lastName, username, password, hoursRemaining } = req.body;
+    if (!firstName || !lastName || !username || !password || !hoursRemaining) {
+      res.status(400).send("Bad Request: All fields are required");
+      return;
+    }
+
     const update = {
       $set: {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        username: req.body.username,
-        password: req.body.password,
-        hoursRemaining: req.body.hoursRemaining
+        firstName: firstName,
+        lastName: lastName,
+        username: username,
+        password: password,
+        hoursRemaining: hoursRemaining
       }
     };
-    try {
-      let db = await mongodb.getDb()
-      const contact = await db.db("CarRentalApp").collection('users').findOne({ _id: userId });
-      if(!contact){
-        res.status(404).send('User not found');
-      }else{
-        const result = await await db.db("CarRentalApp").collection('users').updateOne({ _id: userId }, update);
-        if (result.modifiedCount > 0) {
-            res.status(204).send();
-        } else {
-          res.status(404).send('User not found');
-        }
+    
+    let db = await mongodb.getDb();
+    const contact = await db.db("CarRentalApp").collection('users').findOne({ _id: new ObjectId(userId) });
+    if (!contact) {
+      res.status(404).send("User not found");
+      return;
     }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Error updating user" });
-    }
-  };
 
-  const deleteCurrentUser = async (req, res) => {
-    try {
-      const userId = new ObjectId(req.params.id);
-      let db = await mongodb.getDb()
-      const result = await db.db("CarRentalApp").collection('users').deleteOne({ _id: userId });
-      if (result.deletedCount > 0) {
-        res.status(200).send();
-      } else {
-        res.status(404).send('User not found');
-      }
-    } catch (err) {
-      res.status(500).json(err.message || 'Some error occurred while deleting the user.');
+    const result = await db.db("CarRentalApp").collection('users').updateOne({ _id: new ObjectId(userId) }, update);
+    if (result.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).send("User not found");
     }
-  };
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error updating user" });
+  }
+};
+
+const deleteCurrentUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    if (!ObjectId.isValid(userId)) {
+      res.status(400).send("Invalid user ID");
+      return;
+    }
+
+    let db = await mongodb.getDb();
+    const result = await db.db("CarRentalApp").collection('users').deleteOne({ _id: new ObjectId(userId) });
+    if (result.deletedCount > 0) {
+      res.status(200).send();
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error deleting user" });
+  }
+};
+
 module.exports = { getAllUsers, getSingleUser, createNewUser, updateCurrentUser, deleteCurrentUser };
--- controllers/vehicles.js
+++ controllers/vehicles.js
@@ -2,80 +2,114 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAllVehicles = async (req, res, next) => {
-    let db = await mongodb.getDb()
+  try {
+    let db = await mongodb.getDb();
     const result = db.db("CarRentalApp").collection('vehicles').find();
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
+    const lists = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching vehicles" });
+  }
 };
 
 const getSingleVehicle = async (req, res, next) => {
+  try {
     const userId = new ObjectId(req.params.id);
-    let db = await mongodb.getDb()
+    if (!ObjectId.isValid(userId)) {
+      res.status(400).json({ error: "Invalid user ID" });
+      return;
+    }
+    let db = await mongodb.getDb();
     const result = db.db("CarRentalApp").collection('vehicles').find({ _id: userId });
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0]);
-    });
+    const lists = await result.toArray();
+    if (!lists[0]) {
+      res.status(404).json({ error: "Vehicle not found" });
+      return;
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching vehicle" });
+  }
 };
 
 const createNewVehicle = async (req, res) => {
-    try {
-      const { year, make, model, mileage, remainingRange, currentlyRented, broken } = req.body;
-      const contact = { year, make, model, mileage, remainingRange, currentlyRented, broken };
-      let db = await mongodb.getDb();
-      const result = await db.db("CarRentalApp").collection('vehicles').insertOne(contact);
-      res.status(201).json({ _id: result.insertedId });
-    } catch (error) {
-      res.status(500).json(error).send();
-    }
-  };
-  const updateCurrentVehicle = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
+  try {
     const { year, make, model, mileage, remainingRange, currentlyRented, broken } = req.body;
+    if (!year || !make || !model || !mileage || remainingRange === undefined || currentlyRented === undefined || broken === undefined) {
+      res.status(400).json({ error: "Missing required fields" });
+      return;
+    }
+    const contact = { year, make, model, mileage, remainingRange, currentlyRented, broken };
+    let db = await mongodb.getDb();
+    const result = await db.db("CarRentalApp").collection('vehicles').insertOne(contact);
+    res.status(201).json({ _id: result.insertedId });
+  } catch (error) {
+    res.status(500).json({ error: "Error creating new vehicle" });
+  }
+};
+const updateCurrentVehicle = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    if (!userId) {
+      return res.status(400).send({ error: "Invalid user ID" });
+    }
+
+    const { year, make, model, mileage, remainingRange, currentlyRented, broken } = req.body;
+
+    if (!year || !make || !model || !mileage || !remainingRange || typeof currentlyRented === "undefined" || typeof broken === "undefined") {
+      return res.status(400).send({ error: "All fields are required" });
+    }
+
     const update = {
       $set: {
-        year: req.body.year,
-        make: req.body.make,
-        model: req.body.model,
-        mileage: req.body.mileage,
-        remainingRange: req.body.remainingRange,
-        currentlyRented: req.body.currentlyRented,
-        broken: req.body.broken
+        year: year,
+        make: make,
+        model: model,
+        mileage: mileage,
+        remainingRange: remainingRange,
+        currentlyRented: currentlyRented,
+        broken: broken
       }
     };
-    try {
-      let db = await mongodb.getDb()
-      const vehicle = await db.db("CarRentalApp").collection('vehicles').findOne({ _id: userId });
-      if(!vehicle){
-        res.status(404).send('Vehicle not found');
-      }else{
-        const result = await db.db("CarRentalApp").collection('vehicles').updateOne({ _id: userId }, update);
-        if (result.modifiedCount > 0) {
-            res.status(204).send();
-        } else {
-          res.status(404).send('Vehicle not found');
-        }
-    }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Error updating vehicle" });
-    }
-  };
 
-  const deleteCurrentVehicle = async (req, res) => {
-    try {
-      const userId = new ObjectId(req.params.id);
-      let db = await mongodb.getDb()
-      const result = await db.db("CarRentalApp").collection('vehicles').deleteOne({ _id: userId });
-      if (result.deletedCount > 0) {
-        res.status(200).send();
-      } else {
-        res.status(404).send('User not found');
-      }
-    } catch (err) {
-      res.status(500).json(err.message || 'Some error occurred while deleting the user.');
+    let db = await mongodb.getDb();
+    const vehicle = await db.db("CarRentalApp").collection("vehicles").findOne({ _id: userId });
+    if (!vehicle) {
+      return res.status(404).send({ error: "Vehicle not found" });
     }
-  };
+
+    const result = await db.db("CarRentalApp").collection("vehicles").updateOne({ _id: userId }, update);
+    if (result.modifiedCount > 0) {
+      return res.status(204).send();
+    } else {
+      return res.status(404).send({ error: "Vehicle not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: "Error updating vehicle" });
+  }
+};
+
+const deleteCurrentVehicle = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    if (!userId) {
+      return res.status(400).send({ error: "Invalid user ID" });
+    }
+
+    let db = await mongodb.getDb();
+    const result = await db.db("CarRentalApp").collection("vehicles").deleteOne({ _id: userId });
+    if (result.deletedCount > 0) {
+      return res.status(200).send();
+    } else {
+      return res.status(404).send({ error: "Vehicle not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ error: "Error deleting vehicle" });
+  }
+};
+
 module.exports = { getAllVehicles, getSingleVehicle, createNewVehicle, updateCurrentVehicle, deleteCurrentVehicle };
